a34943e9c1782a4a9825cc209223e3d40f5cf173
Darshi Dholariya <darshidholaria@gmail.com>
2026-05-16 11:31:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const startQuizButton = document.getElementById('startQuiz');
+
+    startQuizButton.addEventListener('click', function() {
+        // Redirect to the quiz page or perform other actions as needed.
+        // You can replace the line below with your quiz URL.
+        window.location.href = 'index.html';
+    });
+});
